5d40ad03ed1b2f6d021a9156aefb75c6a040590b
jaekkim <jaekkim.rim.net>
2018-01-17 17:00:00 -0500

    0117_working on css module issue

---
+++ bootstrapServer.js
@@ -0,0 +1,10 @@
+const path = require('path')
+
+const hook = require('css-modules-require-hook')
+const projectDir = path.resolve(__dirname)
+hook({
+    generateScopedName: '[name]_[local]__[hash:base64:5]',
+    rootDir: projectDir
+})
+
+require('./server')
