541b9a3c51abdb2e5ee9aea79d1bd0ae14459152
jaekkim <jaekkim.rim.net>
2018-02-20 11:40:28 -0500

    0220

---
+++ src/ui/Body/VideoMain.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default class VideoMain extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        <h1>VideoMain</h1>
+    }
+}
